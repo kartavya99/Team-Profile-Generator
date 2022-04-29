d26a51f7f6c3f01b36bd71040edd5e9afcc0c21a
kartavya <kartavyas@gmail.com>
2022-04-29 19:03:11 +1000

    engineer js and test files updated

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,2 +1,13 @@
-const Employee = require("../lib/employee");
-const Manager = require("../lib/engineer");
+const Engineer = require("../lib/engineer");
+
+describe("Engineer Class Tests", () => {
+    it("has getRole() as entered role", () =>{
+        const rambo = new Engineer("rambo", "9", "rambo@office.com", "fighter", "rambo123");
+        expect(rambo.getRole()).toBe("fighter");
+    });
+
+    it("has getGithub() as github username", () => {
+        const rambo = new Engineer("rambo", "9", "rambo@office.com", "fighter", "rambo123");
+        expect(rambo.getGithub()).toBe("rambo123");
+    });
+});
