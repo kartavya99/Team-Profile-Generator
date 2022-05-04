ab7dfa7cb701787de56c3bfd445664cf69ed3f42
kartavya <kartavyas@gmail.com>
2022-05-04 21:12:11 +1000

    index.html added and line break added to space between entry of same employee

--- dist/generatedHTML.js
+++ dist/generatedHTML.js
@@ -2,26 +2,33 @@ const fs = require('fs');
 //const manager = require("../src");
 function generateManagers(manager){
     return `
-    
-   
-    ${manager.name}
-    ${manager.id}
-    ${manager.email}
-    ${manager.officeNumber}
-    
-        
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+         Manager : ${manager.name}
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Employee Id : ${manager.id}</li>
+        <li class="list-group-item">Email :  ${manager.email}</li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+    </ul>
+    </div>
+    <br>
     `
-
 }
 
 function generateEngineers (engineer) {
     return `
-    
-    
-    ${engineer.name}
-    ${engineer.email}
-    ${engineer.id}
-    ${engineer.github}
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+      Engineer : ${engineer.name} 
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Employee Id : ${engineer.id}</li>
+      <li class="list-group-item">Email : ${engineer.email}</li>
+      <li class="list-group-item">Github : ${engineer.github}</li>
+    </ul>
+  </div>
+  <br>
     
     ` 
 }
@@ -29,10 +36,18 @@ function generateEngineers (engineer) {
 function generateInterns (intern) {
 
     return `
-    ${intern.name}
-    ${intern.email}
-    ${intern.id}
-    ${intern.shool}
+
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+      Intern : ${intern.name}
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Employee Id : ${intern.id}</li>
+      <li class="list-group-item">Email : ${intern.email}</li>
+      <li class="list-group-item">School : ${intern.shool}</li>
+    </ul>
+  </div>
+  <br>
 
     `
 }
@@ -40,17 +55,46 @@ function generateInterns (intern) {
 
 function generatedHTML (managers, engineers, interns) {
     return`
-        Manager : 
-        ${managers.map(generateManagers).join("/n")}
 
-        Engineer :
-        ${engineers.map(generateEngineers).join("/n")}
 
-        Interns:
-        ${interns.map(generateInterns).join("/n")}
-            
-    
-    `;
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../dist/style.css">
+        <title>Team Profil Generator</title>
+
+    </head>
+    <body>
+    <header>
+            <h1>Team Profil Generator</h1>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                <h2>Managers</h2>
+                <hr>
+                ${managers.map(generateManagers).join("\n")}
+                </div>
+                <div class="col">
+                <h2>Engineers</h2>
+                <hr>
+                ${engineers.map(generateEngineers).join("\n")}
+                </div>
+                <div class="col">
+                <h2>Interns</h2>
+                <hr>
+                ${interns.map(generateInterns).join("\n")}   
+                </div>
+            </div>
+        </div>
+   
+    </body>
+    </html>
+    `
     
 };
 
