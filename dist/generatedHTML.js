64e9f5d0871290968905d45ccc18b12af2c3d7ac
kartavya <kartavyas@gmail.com>
2022-05-04 18:18:53 +1000

    generatedHTML updated with managers and genHTML function

--- dist/generatedHTML.js
+++ dist/generatedHTML.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 //const manager = require("../src");
+function generateManagers(manager){
+    return `${manager.role}
+    ${manager.name}
+    ${manager.email}
+    ${manager.email}
+    ${manager.id}
+    ${manager.officeNumber}`
+}
 
 function generatedHTML (managers, engineers, interns) {
     return`
 
-   
+
+            ${managers.map(generateManagers).join("/n")}
+            
     
-
-   
-   
-     
-
-    `
+    `;
+    
 };
 
 
