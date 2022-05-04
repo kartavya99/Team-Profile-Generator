b220200224cfc1732a038a514745dcb2f2790d3a
kartavya <kartavyas@gmail.com>
2022-05-04 22:05:32 +1000

    modified styles

--- dist/generatedHTML.js
+++ dist/generatedHTML.js
@@ -64,13 +64,13 @@ function generatedHTML (managers, engineers, interns) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="../dist/style.css">
-        <title>Team Profil Generator</title>
+        <link rel="stylesheet" href="./dist/style.css">
+        <title>Team Profile Generator</title>
 
     </head>
     <body>
     <header>
-            <h1>Team Profil Generator</h1>
+            <h1>Team Profile Generator</h1>
         </header>
         <div class="container">
             <div class="row">
